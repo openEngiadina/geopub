3176e856587ff071d5e71ea2f24617cc669057a4
pukkamustard <pukkamustard@posteo.net>
2019-09-23 15:10:07 +0200

    Leaflet

--- src/figmacs/main.cljs
+++ src/figmacs/main.cljs
@@ -13,13 +13,21 @@
 
 (def Map (r/adapt-react-class react-leaflet/Map))
 (def TileLayer (r/adapt-react-class react-leaflet/TileLayer))
+(def Marker (r/adapt-react-class react-leaflet/Marker))
+(def Popup (r/adapt-react-class react-leaflet/Popup))
+(def CircleMarker (r/adapt-react-class react-leaflet/CircleMarker))
 
 (def server-url "http://localhost:8080/")
+
+(def default-center
+  ;; Default center of map is Scuol
+  [46.8 10.283333])
 
 (defonce state
   (r/atom {:actor-id (str server-url "actors/alice")
            :actor {}
-           :active-page :timeline}))
+           :active-page :timeline
+           :latlng default-center}))
 
 (defn http-get [url]
   (http/get url {:with-credentials? false}))
@@ -63,11 +71,17 @@
 
 (defn activity-component [activity]
   [:div.activity
+   {:class (if (== (:selected-activity @state) (:id activity))
+             ["selected"]
+             [])
+    :on-click #(swap! state assoc :selected-activity (:id activity))}
    [:div.meta
-    [:p 
+    [:p
      [:span.actor (:actor activity)]
      [:span.published (.fromNow (js/moment (:published activity)))]]]
-   [object-component (:object activity)]])
+   [object-component (:object activity)]
+   ;; [:details [:code (prn-str (:object activity))]]
+])
 
 (defn inbox-component [inbox]
   [:div
@@ -87,19 +101,28 @@
   (let [note-content (r/atom "")
 
         create-note (fn [content] {:type "Note" :content content})
+        add-location (fn [object] (merge object
+                                         {:location
+                                          {"@type" "Place"
+                                           :geo {"@type" "GeoCoordinates"
+                                                 :latitude (get-in @state [:latlng :lat])
+                                                 :longitude (get-in @state [:latlng :lng])}}}))
+
         create-activity (fn [to object] {:type "Create" :to to :object object})
 
         stop #(reset! note-content "")
         save #(do (if on-save
                     (->> @note-content
                          create-note
+                         add-location
                          (create-activity ["https://www.w3.org/ns/activitystreams#Public"])
                          on-save))
                   (stop))]
 
     (fn [on-save]
       [:div#create-note
-       [:input#text-input {:type "text"
+       [:h3 "Create a Note"]
+       [:input.text-input {:type "text"
                            :placeholder "A note ..."
                            :value @note-content
                            :on-change #(reset! note-content (-> % .-target .-value))
@@ -107,7 +130,13 @@
                                            13 (save)
                                            27 (stop)
                                            nil)}]
-       [:input#send-button {:type "button" :value "Send" :on-click save}]])))
+       [:p.latlng (str "Position: "
+                       (get-in @state [:latlng :lat])
+                       " / "
+                       (get-in @state [:latlng :lng])
+                       " (Click on map to update)")]
+
+       [:input.send-button {:type "button" :value "Create" :on-click save}]])))
 
 (defn nav-bar [active-page]
   (let
@@ -122,6 +151,7 @@
       (nav-element :timeline)
       (nav-element :events)
       (nav-element :tours)
+      (nav-element :curated)
       (nav-element :system)]]))
 
 (defn system-page []
@@ -153,9 +183,10 @@
            [:hr]])
         (get-in @state [:public :items]))])
 
-(def default-center
-  ;; Default center of map is Scuol
-  [46.8, 10.283333])
+(defn object-location [object]
+  (when (get-in object [:location :geo])
+    [(get-in object [:location :geo :latitude])
+     (get-in object [:location :geo :longitude])]))
 
 (defn ap-demo-app []
   [:div#container
@@ -174,18 +205,47 @@
 
       :tours "TODO"
 
-      :system [system-page])]
+      :curated "TODO"
+
+      :system [system-page]
+
+      "404")]
 
    [:div#mapid
     [Map
      {:style {:min-height "98vh"}
       :center default-center
       :zoom 12
-      :ref (fn [el] (prn el))
-      :on-click (fn [el] (prn el))}
+      :on-click (fn [e]
+                  (let [latlng (js->clj (.-latlng e))]
+                    (swap! state #(assoc % :latlng {:lat (get latlng "lat")
+                                                    :lng (get latlng "lng")}))))}
      [TileLayer
       {:url osm-url
-       :attribution copy-osm}]]]])
+       :attribution copy-osm}]
+
+     (for [activity (get-in @state [:public :items])]
+       (let [id (:id activity)
+             location (object-location (:object activity))]
+         (when location
+           [Marker {:position location
+                    :id id
+                    :on-click #(swap! state assoc :selected-activity id)
+                    :on-mouse-over #(swap! state assoc :hover-activity id)
+                    :on-mouse-out #(swap! state assoc :hover-activity nil)}
+
+            [Popup
+             [object-component (:object activity)]]
+
+            ;; (when (== (:selected-activity @state) (:id activity))
+            ;;   [CircleMarker
+            ;;    {:center location
+            ;;     :radius 10}
+            ;;    ])
+
+            ])))
+
+]]])
 
 (r/render [ap-demo-app]
           (js/document.getElementById "app")
