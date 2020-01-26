5457f85ce2c0cdc40b26ce85a492ac09b37dca95
pukkamustard <pukkamustard@posteo.net>
2020-01-26 10:16:59 +0100

    cljs-rdf: pretty printer for n3 types

--- src/cljs_rdf/core.cljs
+++ src/cljs_rdf/core.cljs
@@ -37,9 +37,21 @@
 (extend-type n3-quad-type
   Quad
   (quad-subject [x] (.-subject x))
-  (quad-predicate [x] (.-subject x))
-  (quad-object [x] (.-subject x))
-  (quad-graph [x] (.-subject x)))
+  (quad-predicate [x] (.-predicate x))
+  (quad-object [x] (.-object x))
+  (quad-graph [x] (.-graph x))
+
+  IPrintWithWriter
+  (-pr-writer [o w _]
+    (write-all w
+               "#Quad {"
+               "subject: " (prn-str (quad-subject o))
+               ", predicate: " (prn-str (quad-predicate o))
+               ", object: " (prn-str (quad-object o))
+               ", graph: " (prn-str (quad-graph o))
+               "}"
+               )))
+
 
 (def n3-term-type (.-Term (.-internal (.-DataFactory n3))))
 
@@ -52,13 +64,20 @@
 
 (extend-type n3-named-node-type
   NamedNode
-  (named-node-iri [x] (.-value x)))
+  (named-node-iri [x] (.-value x))
+
+  IPrintWithWriter
+  (-pr-writer [o w _] (write-all w "<" (named-node-iri o) ">")))
 
 (def n3-blank-node-type (.-BlankNode (.-internal (.-DataFactory n3))))
 
 (extend-type n3-blank-node-type
   BlankNode
-  (blank-node-id [x] (.-value x)))
+  (blank-node-id [x] (.-value x))
+
+  IPrintWithWriter
+  (-pr-writer [o w _] (write-all w "_:" (prn-str (blank-node-id o))))
+  )
 
 (def n3-literal-type (.-Literal (.-internal (.-DataFactory n3))))
 
@@ -66,7 +85,11 @@
   Literal
   (literal-value [x] (.-value x))
   (literal-language [x] (.-language x))
-  (literal-datatype [x] (.-datatype x)))
+  (literal-datatype [x] (.-datatype x))
+
+  IPrintWithWriter
+  ;; TODO also print datatype and language
+  (-pr-writer [o w _] (write-all w "\"" (pr-str (literal-value o)) "\"")))
 
 (def n3-variable-type (.-Variable (.-internal (.-DataFactory n3))))
 
