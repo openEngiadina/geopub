67e98b59b15e4de08573a8cbc1f6ee4f78ebcd96
pukkamustard <pukkamustard@posteo.net>
2020-03-17 15:59:34 +0100

    geopub.ui.browse: use reagent track to rerender when graph changes

--- src/geopub/ui/browse.cljs
+++ src/geopub/ui/browse.cljs
@@ -1,6 +1,7 @@
 (ns geopub.ui.browse
   (:require [rdf.core :as rdf]
             [rdf.description :as rdf-description]
+            [reagent.core :as r]
             [goog.string]
             [geopub.data.rdf :refer [description-header-component
                                      description-component]]))
@@ -11,8 +12,9 @@
                 (get-in [:current-route :path-params :iri])
                 (goog.string.urlDecode)
                 (rdf/iri))
-        description (rdf-description/description iri (:graph @state))]
+        
+        description (r/track #(rdf-description/description iri (:graph @state)))]
 
     [:div {:id :browse}
-     [description-component description]]))
+     [description-component @description]]))
 
