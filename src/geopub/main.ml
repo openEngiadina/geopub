724f11358b2a0d9c52f9101164be4f1600d12722
pukkamustard <pukkamustard@posteo.net>
2021-09-01 15:49:00 +0200

    Leaflet: Bindings to the Javascript Leaflet library

--- src/geopub/main.ml
+++ src/geopub/main.ml
@@ -7,12 +7,33 @@ open Lwt
 open Brr
 open Brr_react
 open Lwt_react
-open Js_of_ocaml_lwt
+(* open Js_of_ocaml_lwt *)
 
-let body, set_body = S.create []
+type model = El.t
 
-let _body_update_s = Elr.def_children (Document.body G.document) body
+let model_s, update_model = S.create @@ El.div ~at:At.[ id (Jstr.v "map") ] []
 
-let main = Lwt_js.sleep 1.0 >|= fun () -> set_body El.[ txt' "Hello world!" ]
+let topbar =
+  El.(
+    div
+      ~at:At.[ id (Jstr.v "topbar") ]
+      [
+        header [ img ~at:At.[ src (Jstr.v "sgraffito.svg") ] () ];
+        nav [ ul [ li [ txt' "Hi" ] ] ];
+        nav [ ul [ li [ txt' "About" ] ] ];
+      ])
+
+let view model = El.[ topbar; div ~at:At.[ id (Jstr.v "main") ] [ model ] ]
+
+let main =
+  let _view_s =
+    Elr.def_children (Document.body G.document) S.(map view model_s)
+  in
+  let map_container = S.value model_s in
+  let map = Leaflet.create map_container in
+  Leaflet.(set_view map LatLng.(create 63.4275 10.4109) 11);
+  let tile_layer = Leaflet.TileLayer.create_osm () in
+  Leaflet.TileLayer.add_to map tile_layer;
+  return_unit
 
 let () = ignore main
---
+++ src/leaflet/leaflet.ml
@@ -0,0 +1,49 @@
+(*
+ * SPDX-FileCopyrightText: 2021 pukkamustard <pukkamustard@posteo.net>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ *)
+open Brr
+
+let leaflet =
+  match Jv.(find global "L") with
+  | Some l -> l
+  | None -> failwith "Could not load Leaflet"
+
+type t = Jv.t
+
+let create el = Jv.call leaflet "map" [| El.to_jv el |]
+
+let invalidate_size map = ignore @@ Jv.call map "invalidateSize" [| Jv.true' |]
+
+module LatLng = struct
+  type t = Jv.t
+
+  let create lat lng =
+    Jv.call leaflet "latLng" [| Jv.of_float lat; Jv.of_float lng |]
+end
+
+let set_view map latlng zoom =
+  ignore @@ Jv.call map "setView" [| latlng; Jv.of_int zoom |]
+
+module TileLayer = struct
+  type map = t
+
+  type t = Jv.t
+
+  let create_osm () =
+    Jv.call leaflet "tileLayer"
+      [|
+        Jv.of_string "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+        Jv.obj
+          [|
+            ( "attribution",
+              Jv.of_string
+                "&copy; <a \
+                 href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> \
+                 contributors" );
+          |];
+      |]
+
+  let add_to map tile_layer = ignore @@ Jv.call tile_layer "addTo" [| map |]
+end
