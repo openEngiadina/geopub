aa7fe6dba86718e39426e6ba9b8188afd1519f38
pukkamustard <pukkamustard@posteo.net>
2021-09-13 10:14:48 +0200

    Brr_react: Add Evr (taken directly from Brr_note)

--- src/brr_react/brr_react.ml
+++ src/brr_react/brr_react.ml
@@ -4,8 +4,34 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  *)
 
+(* Parts are taken from brr_note.el which is part of the Brr package
+ * provided under the ISC license. *)
+
 open Brr
 open React
+
+module Evr = struct
+  let instruct ?(propagate = true) ?(default = true) e =
+    if default then () else Ev.prevent_default e;
+    if propagate then () else Ev.stop_propagation e
+
+  let on_target ?(capture = false) ?propagate ?default type' f t =
+    let opts =
+      match capture with
+      | false -> None
+      | true -> Some (Ev.listen_opts ~capture ())
+    in
+    let e, send_e = E.create () in
+    let f ev =
+      instruct ?propagate ?default ev;
+      send_e (f ev)
+    in
+    Ev.listen ?opts type' f t;
+    e
+
+  let on_el ?capture ?propagate ?default type' f el =
+    on_target ?capture ?propagate ?default type' f (El.as_target el)
+end
 
 module Elr = struct
   let def_children e cs = cs |> S.map (El.set_children e)
--- src/brr_react/brr_react.mli
+++ src/brr_react/brr_react.mli
@@ -7,6 +7,26 @@
 open Brr
 open React
 
+module Evr : sig
+  val on_target :
+    ?capture:bool ->
+    ?propagate:bool ->
+    ?default:bool ->
+    'b Ev.type' ->
+    ('b Ev.t -> 'c) ->
+    Ev.target ->
+    'c event
+
+  val on_el :
+    ?capture:bool ->
+    ?propagate:bool ->
+    ?default:bool ->
+    'b Ev.type' ->
+    ('b Ev.t -> 'c) ->
+    El.t ->
+    'c event
+end
+
 module Elr : sig
   val def_children : El.t -> El.t list signal -> unit signal
 end
