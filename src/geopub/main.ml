bad6591bae5b069d888855e89d2e2b8fb3839663
pukkamustard <pukkamustard@posteo.net>
2021-09-13 12:27:39 +0200

    Geopub: Experimenting with how to structure an application.
    Inspired by todomvc.ml from Brr.

--- src/geopub/main.ml
+++ src/geopub/main.ml
@@ -4,8 +4,7 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  *)
 open Lwt
-
-(* open Lwt.Syntax *)
+open Lwt.Syntax
 open Brr
 open Brr_react
 open Lwt_react
@@ -19,43 +18,20 @@ module Log = (val Logs_lwt.src_log src : Logs_lwt.LOG)
 
 type route = About | Map | Messages
 
+type route_msg = [ `SetRoute of route ]
+
+type msg = route_msg
+
 type model = { route : route; map : Geopub_map.t }
+(* { route : route; map : Geopub_map.t; xmpp : Geopub_xmpp.t } *)
 
-let init () = { route = About; map = Geopub_map.create () }
-
-let model_s, update_model = S.create @@ init ()
-
-let update_route route =
-  let model = S.value model_s in
-  update_model { model with route }
+let init () = return { route = About; map = Geopub_map.create () }
+(* let* xmpp = Geopub_xmpp.init () in
+ * return { route = About; map = Geopub_map.create (); xmpp } *)
 
 (* View *)
 
-module Topbar = struct
-  let make_entry name route =
-    let a_el = El.(a ~at:At.[ href @@ Jstr.v "#" ] [ txt' name ]) in
-    Ev.listen Ev.click
-      (function _ev -> update_route route)
-      (El.as_target a_el);
-    El.(li [ a_el ])
-
-  let view () =
-    El.(
-      div
-        ~at:At.[ id (Jstr.v "topbar") ]
-        [
-          header [ img ~at:At.[ src (Jstr.v "sgraffito.svg") ] () ];
-          nav
-            [
-              ul
-                [
-                  make_entry "Map" Map;
-                  make_entry "Messages" Messages;
-                  make_entry "About" About;
-                ];
-            ];
-        ])
-end
+module Topbar = struct end
 
 let about_view =
   El.(
@@ -92,17 +68,63 @@ let about_view =
           ];
       ])
 
-let view model =
-  let main_view = function
-    | About -> about_view
-    | Messages -> El.txt' "GeoPub is also an XMPP client!"
-    | Map -> Leaflet.get_container model.map
+let topbar _model_s =
+  let make_entry name route =
+    let a_el = El.(li [ a ~at:At.[ href @@ Jstr.v "#" ] [ txt' name ] ]) in
+    (a_el, Evr.on_el Ev.click (function _ev -> `SetRoute route) a_el)
   in
-  El.
+  let entries =
     [
-      Topbar.view ();
-      div ~at:At.[ id (Jstr.v "main") ] [ main_view model.route ];
+      make_entry "Map" Map;
+      make_entry "Messages" Messages;
+      make_entry "About" About;
     ]
+  in
+  let e = entries |> List.map snd |> E.select in
+  ( e,
+    El.(
+      div
+        ~at:At.[ id (Jstr.v "topbar") ]
+        [
+          header [ img ~at:At.[ src (Jstr.v "sgraffito.svg") ] () ];
+          nav [ ul List.(map fst entries) ];
+        ]) )
+
+let main model_s =
+  let main_el = El.div ~at:At.[ id @@ Jstr.v "main" ] [] in
+  let msg_e, _ = E.create () in
+  let def_signal =
+    Elr.def_children main_el
+      (S.map
+         (fun model ->
+           match model.route with
+           | Map -> [ Leaflet.get_container model.map ]
+           | Messages -> El.[ txt' "Messages" ]
+           | About -> [ about_view ])
+         model_s)
+  in
+  (def_signal, msg_e, main_el)
+
+let update msg model = match msg with `SetRoute r -> { model with route = r }
+
+let ui init_model =
+  let def model_s =
+    let main_def_s, main_msg, main = main model_s in
+    let topbar_msg, topbar = topbar model_s in
+
+    (* Keep reference to all update signals *)
+    S.keep @@ S.merge (fun _ _ -> ()) () [ main_def_s ];
+
+    (* Collect all events *)
+    let msg = E.select [ main_msg; topbar_msg ] in
+
+    (* Perform the model update *)
+    let updates = E.map update msg in
+    let model_s = S.accum updates (S.value model_s) in
+    (model_s, [ topbar; main ])
+  in
+  (* I admit, I do not understand what this does... TODO wrap my head around S.fix *)
+  S.fix init_model def
 
 (* Start the application *)
 
@@ -111,11 +133,8 @@ let setup_logging () =
   Logs.set_level @@ Some Logs.Info
 
 let main =
-  let _view_s =
-    Elr.def_children (Document.body G.document) S.(map view model_s)
-  in
-
-  return_unit
+  let* init_model = init () in
+  return @@ El.set_children (Document.body G.document) (ui init_model)
 
 let () =
   setup_logging ();
