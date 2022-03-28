0de2eb31b5c8d7a6bf2676cca4bf7901adf996c8
pukkamustard <pukkamustard@posteo.net>
2022-03-28 09:06:10 +0200

    GeoPub.Router: parse uri fragments to route and load correct route when reloading.

--- src/geopub/router.ml
+++ src/geopub/router.ml
@@ -7,20 +7,35 @@
 open Brr
 open Lwt_react
 
+(* Setup logging *)
+
+let src = Logs.Src.create "GeoPub.Router"
+
+module Log = (val Logs.src_log src : Logs.LOG)
+
 let history = Window.history G.window
 
 type route = About | Login | Map | Inspect of Rdf.Iri.t
 
 let parser uri =
-  let fragment = Jstr.to_string @@ Uri.fragment uri in
-  match fragment with "about" -> About | "login" -> Login | _ -> About
+  let path =
+    String.split_on_char '/' @@ Option.value ~default:"" @@ Rdf.Iri.fragment uri
+  in
+  match path with
+  | [ "about" ] -> About
+  | [ "login" ] -> Login
+  | [ "map" ] -> Map
+  | [ "inspect"; iri_s ] -> Inspect (Rdf.Iri.of_string iri_s)
+  | _ -> About
+
+let get_location () =
+  Window.location G.window |> Uri.to_jstr |> Jstr.to_string |> Rdf.Iri.of_string
+  |> parser
 
 let update =
   let e, push_state = E.create () in
   Ev.listen Window.History.Ev.popstate
-    (fun _ ->
-      let location = Window.location G.window in
-      push_state @@ parser location)
+    (fun _ -> get_location () |> push_state)
     (Window.as_target G.window);
   e
 
@@ -31,12 +46,12 @@ let to_uri route =
   | Login -> Uri.with_uri location ~fragment:(Jstr.v "login")
   | Map -> Uri.with_uri location ~fragment:(Jstr.v "map")
   | Inspect iri ->
-      Uri.with_uri location ~fragment:(Jstr.v "inspect")
-        ~query:(Jstr.v ("iri=" ^ Rdf.Iri.to_string iri)))
+      Uri.with_uri location
+        ~fragment:(Jstr.v @@ "inspect/" ^ Rdf.Iri.to_string iri))
   |> Result.value ~default:location
 
 let set_route route =
   Window.History.push_state ~uri:(to_uri route) history;
   route
 
-let init () = set_route About
+let init () = get_location ()
