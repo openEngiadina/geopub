78d876f09b757e6e7b6d32a14b780af565b9d41f
pukkamustard <pukkamustard@posteo.net>
2022-06-17 15:11:00 +0200

    GeoPub: Set log levels back to Info

--- src/geopub/main.ml
+++ src/geopub/main.ml
@@ -61,9 +61,8 @@ let main () =
   (* Setup logging *)
   Logs.set_reporter @@ Logs_browser.console_reporter ();
 
-  Logs.set_level @@ Some Logs.Debug;
-
-  (* Logs.set_level @@ Some Logs.Info; *)
+  (* Logs.set_level @@ Some Logs.Debug; *)
+  Logs.set_level @@ Some Logs.Info;
 
   (* Initialize the application *)
   let () = Log.app (fun m -> m "Initializing GeoPub.") in
