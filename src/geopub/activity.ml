56575b866b7ccd7f3ecc966c2a310e165ae61af6
pukkamustard <pukkamustard@posteo.net>
2022-09-15 15:41:16 +0200

    GeoPub.Activity: merge in New_post

--- src/geopub/activity.ml
+++ src/geopub/activity.ml
@@ -8,6 +8,8 @@ open Lwt
 open Lwt.Syntax
 open Lwt_react
 open Brr
+open Brr_io
+open Brr_react
 
 (* Setup logging *)
 
@@ -15,7 +17,206 @@ let src = Logs.Src.create "GeoPub.Activity"
 
 module Log = (val Logs.src_log src : Logs.LOG)
 
-(* Component *)
+(* Namespaces *)
+let activitystreams = Namespace.activitystreams
+let geo = Namespace.geo
+
+(* Content-addressing with ERIS *)
+
+let hash s =
+  let _, read_cap =
+    Eris.(
+      encode_string ~convergence_secret:null_convergence_secret
+        ~block_size:`Small s)
+  in
+  Rdf.Iri.of_string @@ Eris.Read_capability.to_urn read_cap
+
+(* Activity types *)
+
+module Create = struct
+  let make ~object' actor =
+    let object_id = Rdf_cbor.Content_addressable.base_subject ~hash object' in
+    let create_activity =
+      Rdf_cbor.Content_addressable.(
+        empty
+        |> add_statement Namespace.a (Object.of_iri @@ activitystreams "Create")
+        |> add_statement
+             (Predicate.of_iri @@ activitystreams "actor")
+             (Object.of_iri actor)
+        |> add_statement
+             (Predicate.of_iri @@ activitystreams "object")
+             (Object.of_iri object_id)
+        |> add_statement
+             (Predicate.of_iri @@ activitystreams "published")
+             (Object.of_literal
+             @@ Rdf.Literal.make
+                  (Ptime.to_rfc3339 @@ Ptime_clock.now ())
+                  (Rdf.Namespace.xsd "dateTime")))
+    in
+    return
+      ( Rdf_cbor.Content_addressable.base_subject ~hash create_activity,
+        Rdf.Graph.(
+          empty
+          |> add_seq (Rdf_cbor.Content_addressable.to_triples ~hash object')
+          |> add_seq
+               (Rdf_cbor.Content_addressable.to_triples ~hash create_activity))
+      )
+end
+
+module Like = struct
+  let make object_id actor =
+    let like_activity =
+      Rdf_cbor.Content_addressable.(
+        empty
+        |> add_statement Namespace.a (Object.of_iri @@ activitystreams "Like")
+        |> add_statement
+             (Predicate.of_iri @@ activitystreams "actor")
+             (Object.of_iri actor)
+        |> add_statement
+             (Predicate.of_iri @@ activitystreams "object")
+             (Object.of_iri object_id)
+        |> add_statement
+             (Predicate.of_iri @@ activitystreams "published")
+             (Object.of_literal
+             @@ Rdf.Literal.make
+                  (Ptime.to_rfc3339 @@ Ptime_clock.now ())
+                  (Rdf.Namespace.xsd "dateTime")))
+    in
+    return
+      ( Rdf_cbor.Content_addressable.base_subject ~hash like_activity,
+        Rdf.Graph.(
+          empty
+          |> add_seq
+               (Rdf_cbor.Content_addressable.to_triples ~hash like_activity)) )
+end
+
+(* Publish Activities *)
+
+module Publish = struct
+  let actor xmpp_client =
+    Xmpp.Client.jid xmpp_client >|= fun jid ->
+    ("xmpp:" ^ Xmpp.Jid.(to_string @@ bare jid)) |> Rdf.Iri.of_string
+end
+
+(* Object types *)
+
+module Note = struct
+  let make ?latlng content =
+    Rdf_cbor.Content_addressable.(
+      empty
+      |> add_statement
+           (Predicate.of_iri @@ Rdf.Namespace.rdf "type")
+           (Object.of_iri @@ activitystreams "Note")
+      |> add_statement
+           (Predicate.of_iri @@ activitystreams "content")
+           (Object.of_literal @@ Rdf.Literal.make_string content)
+      |> add_opt_statement
+           (Predicate.of_iri @@ geo "lat")
+           (Option.map
+              (fun latlng ->
+                Object.of_literal @@ Rdf.Literal.make_string @@ Float.to_string
+                @@ Leaflet.Latlng.lat latlng)
+              latlng)
+      |> add_opt_statement
+           (Predicate.of_iri @@ geo "long")
+           (Option.map
+              (fun latlng ->
+                Object.of_literal @@ Rdf.Literal.make_string @@ Float.to_string
+                @@ Leaflet.Latlng.lng latlng)
+              latlng))
+
+  let view post =
+    S.map (fun busy ->
+        El.(
+          Evf.on_el ~default:false Form.Ev.submit (fun ev ->
+              (* let form = Ev.(target_to_jv @@ target ev) in *)
+              let form_data =
+                Form.Data.of_form @@ Form.of_jv @@ Ev.target_to_jv
+                @@ Ev.target ev
+              in
+
+              Console.log [ form_data ];
+
+              let content_value =
+                Form.Data.find form_data (Jstr.v "content") |> Option.get
+              in
+
+              let content =
+                match content_value with
+                | `String js -> Jstr.to_string js
+                | _ -> failwith "We need better error handling"
+              in
+
+              let note = make content in
+              ignore @@ post note)
+          @@ form
+               ~at:[ UIKit.Form.stacked; UIKit.margin ]
+               [
+                 (* Content *)
+                 textarea
+                   ~at:
+                     At.
+                       [
+                         UIKit.Form.textarea;
+                         UIKit.Form.controls;
+                         id @@ Jstr.v "content";
+                         name @@ Jstr.v "content";
+                       ]
+                   [];
+                 (* Post *)
+                 div ~at:[ UIKit.margin ]
+                   [
+                     input
+                       ~at:
+                         At.(
+                           add_if busy disabled
+                             [
+                               UIKit.Form.input;
+                               UIKit.Button.primary;
+                               id @@ Jstr.v "submit";
+                               type' @@ Jstr.v "submit";
+                               value @@ Jstr.v "Post";
+                             ])
+                       ();
+                   ];
+               ]))
+end
+
+module Compose = struct
+  let view xmpp_client xmpp_rdf =
+    let busy_s, set_busy = S.create false in
+
+    let* actor = Publish.actor xmpp_client in
+
+    let post object' =
+      let* id, graph = Create.make ~object' actor in
+      set_busy true;
+      Xmpp_rdf.Publish.to_activitystreams_pep xmpp_rdf id graph >|= fun _ ->
+      set_busy false
+    in
+
+    return
+    @@ S.map
+         (fun compose_el ->
+           El.
+             [
+               div ~at:[ UIKit.container ]
+                 [
+                   h3 [ txt' "New Post" ];
+                   ul ~at:[ UIKit.subnav ]
+                     [
+                       li ~at:[ UIKit.active ]
+                         [ a [ txt' "ActivityStreams Note" ] ];
+                       li [ a [ txt' "ValueFlows Proposal" ] ];
+                       li [ a [ txt' "RDF" ] ];
+                     ];
+                   compose_el;
+                 ];
+             ])
+         (Note.view post busy_s)
+end
+
+(* Query for Activities *)
 
 module TermSet = Set.Make (Rdf.Term)
 
@@ -125,7 +326,7 @@ let view_activity db description =
                        li [ object_el ];
                        li [ published ];
                        li ~at:[ UIKit.Width.expand ] [];
-                       li [ a ~at:[ UIKit.Icon.comment ] [] ];
+                       li [ a ~at:[ UIKit.Icon.star ] [] ];
                        (* li [ a ~at:[] [ txt' "Like" ] ]; *)
                      ];
                  ];
@@ -144,7 +345,7 @@ let view xmpp xmpp_rdf db =
         match xmpp_client with
         | Some xmpp_client ->
             (* div ~at:[ UIKit.section; UIKit.Section.muted; UIKit.padding ] *)
-            New_post.view xmpp_client xmpp_rdf
+            Compose.view xmpp_client xmpp_rdf
         | None ->
             return @@ S.const
             @@ El.
